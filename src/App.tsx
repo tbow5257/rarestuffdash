3db47566f4ac04bbefc743b9a66c541f12e70a76
Thomas Bowen <tjbowen.52@gmail.com>
2020-07-09 19:31:01 -0700

    load music data from GQL endpoint and display in material grid list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  gql,
+  ApolloProvider
+} from "@apollo/client";
+
+import Wut from './Wut';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://127.0.0.1:8000/graphql",
+    credentials: "same-origin",
+  }),
+});
+
+const ALL_ALBUMS = gql`
+  query {
+    allAlbums {
+      edges {
+        node {
+          name
+          releaseId
+          price
+          style
+          have
+          want
+        }
+      }
+    }
+  }
+`;
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Wut />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/Wut.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import orderBy from "lodash/orderBy";
+import { makeStyles } from "@material-ui/core/styles";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import Table from "@material-ui/core/Table";
+import Paper from '@material-ui/core/Paper';
+
+const ALL_ALBUMS = gql`
+  query {
+    allAlbums {
+      edges {
+        node {
+          id
+          name
+          releaseId
+          price
+          style
+          have
+          want
+          style
+        }
+      }
+    }
+  }
+`;
+
+interface Edge {
+  node: Node;
+}
+
+interface Node {
+  id: string;
+  releaseId: string;
+  name: string;
+  style: string;
+  want: number;
+  have: number;
+  price: number;
+}
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+    width: '100%',
+    tableLayout: 'fixed',
+  },
+});
+
+const transformToDisplayPrice = (wholeNum: number) => (wholeNum / 100).toFixed(2);
+
+export default function Wut() {
+  const classes = useStyles();
+
+  const { loading, error, data } = useQuery(ALL_ALBUMS);
+
+  if (loading) return <div>Load time</div>;
+
+  const meOrdered = orderBy(
+    data.allAlbums.edges,
+    (edge) => edge.node.want,
+    "desc"
+  );
+  console.log("mo", meOrdered);
+
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} >
+          <TableBody>
+            {meOrdered.map((edge: Edge) => (
+              <TableRow>
+                <TableCell>{edge.node.releaseId}</TableCell>
+                <TableCell>{edge.node.name}</TableCell>
+                <TableCell>Want:{edge.node.want}</TableCell>
+                <TableCell>Have:{edge.node.have}</TableCell>
+                <TableCell>price: ${transformToDisplayPrice(edge.node.price)}</TableCell>
+                <TableCell>{edge.node.style}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
