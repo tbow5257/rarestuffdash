bbccb505d064c7f819e65c24ccb725cbedca296c
Thomas Bowen <tjbowen.52@gmail.com>
2020-07-09 19:59:09 -0700

    header row

--- src/Wut.tsx
+++ src/Wut.tsx
@@ -8,7 +8,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import Table from "@material-ui/core/Table";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 const ALL_ALBUMS = gql`
   query {
@@ -43,15 +43,18 @@ interface Node {
   price: number;
 }
 
+const headCells: string[] = ["releaseId", "name", "want", "have", "price", "style"];
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
-    width: '100%',
-    tableLayout: 'fixed',
+    width: "100%",
+    tableLayout: "fixed",
   },
 });
 
-const transformToDisplayPrice = (wholeNum: number) => (wholeNum / 100).toFixed(2);
+const transformToDisplayPrice = (wholeNum: number) =>
+  (wholeNum / 100).toFixed(2);
 
 export default function Wut() {
   const classes = useStyles();
@@ -70,7 +73,14 @@ export default function Wut() {
   return (
     <div>
       <TableContainer component={Paper}>
-        <Table className={classes.table} >
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              {headCells.map((headCell) => (
+                <TableCell>{headCell}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
           <TableBody>
             {meOrdered.map((edge: Edge) => (
               <TableRow>
@@ -78,7 +88,9 @@ export default function Wut() {
                 <TableCell>{edge.node.name}</TableCell>
                 <TableCell>Want:{edge.node.want}</TableCell>
                 <TableCell>Have:{edge.node.have}</TableCell>
-                <TableCell>price: ${transformToDisplayPrice(edge.node.price)}</TableCell>
+                <TableCell>
+                  price: ${transformToDisplayPrice(edge.node.price)}
+                </TableCell>
                 <TableCell>{edge.node.style}</TableCell>
               </TableRow>
             ))}
